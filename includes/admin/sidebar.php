cc36fc44abbaf217d39ac19e826de9442b8bb323
nourabuelenin <nourabuelenin@gmail.com>
2025-02-13 00:15:59 +0200

    after merge cleanup

--- includes/admin/sidebar.php
+++ includes/admin/sidebar.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <!-- Bootstrap CSS&script -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-    <link rel="stylesheet" href="../../assets/admin/css/style.css">
-</head>
+
 <body>
     <div class="sidebar animate__animated animate__fadeInLeft">
         <h4 class="text-center">Admin Dashboard</h4>
