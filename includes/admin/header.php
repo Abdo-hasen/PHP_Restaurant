dd915f83ea33e0c833a84386a3bd1a67e44253d6
nourabuelenin <nourabuelenin@gmail.com>
2025-02-15 21:36:14 +0200

    Dashboard logic fixed & header and sidebar modified.

--- includes/admin/header.php
+++ includes/admin/header.php
@@ -7,7 +7,28 @@
     <!-- Bootstrap CSS&script -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link rel="stylesheet" href="../../assets/admin/css/style.css">
 </head>
+
+<nav class="navbar border-bottom border-body">  
+<div class="container-fluid">
+    <a class="navbar-brand">
+        <?php
+        if (isset($_SESSION['admin_username'])) {
+            echo "Welcome, " . $_SESSION['admin_username'];
+        } else {
+            echo "Welcome, Admin";
+        }
+        ?>
+    </a>
+    <div class="d-flex">
+      <a href="../../profile.php" class="nav-link">
+        <i class="bi bi-person-circle"></i>
+      </a>
+    </div>
+  </div>
+</nav>
+
